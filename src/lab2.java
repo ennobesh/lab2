f5e923023d47d1ce439ccc1954f01691f920975e
ennonbesh <akusepskksng@gmsil.com>
2021-01-26 21:48:35 +0800

    JavaApplication1

---
+++ src/lab2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author akuse
+ */
+public class lab2 {
+    public static void main (String args[]) {
+		System.out.println("Number\t SquareRoot\n");
+    for (int i=0; i <= 20; i=i+2) {
+      System.out.printf("%d \t %.4f\n", i, Math.sqrt(i)); 
+  	}
+  }
+}
